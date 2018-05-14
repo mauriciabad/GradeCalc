61a4274153b02935a13e43c60693c7990f9e837b
Maurici Abad <hello@mauriciabad.com>
2018-05-15 01:22:23 +0200

    Edition UI improved

--- sctipt.js
+++ sctipt.js
@@ -3,9 +3,38 @@ var subjectCookies = [];
 
 //loadData();
 createSubjectCardCollapsed({
+  "id": "H47SF628H",
+  "name": "AC",
+  "finalMark": 3.92,
+  "grades": {
+    "Teoria": {
+      "C1": 6.3,
+      "C2": 5.5,
+      "C3": null
+    },
+    "Laboratorio": {
+      "L": 8
+    }
+  },
+  "evaluation": {
+    "Teoria": {
+      "C1": 0.15,
+      "C2": 0.25,
+      "C3": 0.4
+    },
+    "Laboratorio": {
+      "L": 0.2
+    }
+  },
+  "color": 4,
+  "uni": "UPC",
+  "faculty": "FIB"
+}
+);
+createSubjectCardCollapsed({
   "id": "2E4TDt64s",
   "name": "IES",
-  "finalMark": 2.925,
+  "finalMark": 2.93,
   "grades": {
     "Teoria": {
       "T1": 5.5,
@@ -50,41 +79,42 @@ function createSubjectCardCollapsed(subject) {
   card.className = 'subject-card collaped-sc';
 
   card.innerHTML = '<h2>' + subject.name +
-  '</h2><p style="color: ' + (subject.finalMark>=5 ? '#008000' : '#C0392B') + 
-  ';">' + subject.finalMark + '</p><div class="subject-bar">';
+  '</h2><p style="color: ' + (subject.finalMark>=5 ? '#5a9764' : '#b9574c') + 
+  ';">' + subject.finalMark + '</p><div class="subject-bar"></div><div class="grades-input"></div>';
 
-  var necessaryMark;
+  var necessaryMark=0;
   if (subject.finalMark < 5) necessaryMark = round(gradeCalcAllEqual(subject.grades, subject.evaluation, subject.finalMark));
-  else necessaryMark = 0;
 
   for (const typeExam in subject.grades) {
+    card.children[3].innerHTML += '<h3>' + typeExam + '</h3><div></div>';
+    
     for (const exam in subject.grades[typeExam]) {
-      var mark = subject.grades[typeExam][exam];
-      var isNull = mark === null;
+      let mark = subject.grades[typeExam][exam];
+      let isNull = mark === null;
       
       if (isNull) {
         mark = necessaryMark;
       }
-
+      
       card.children[2].innerHTML += '<div class="scol' + (isNull ? 'N' : subject.color) + '" style="flex-grow: ' + subject.evaluation[typeExam][exam]*100 + 
       '">' + exam + '<div>' + mark + '</div></div>';
-
+      card.children[3].lastChild.innerHTML += '<div><span>'+ exam +':</span><input type="number" placeholder="'+ necessaryMark +'" value="'+ subject.grades[typeExam][exam] +'" class="scol' + (isNull ? 'N2' : subject.color) + '" autocomplete="off"></div>';
     }
   }
 
-  card.innerHTML += '</div></div>';
+  card.innerHTML += '</div>';
 
   dashboard.appendChild(card);
 }
 
 function gradeCalcAllEqual(grades, evaluation, finalMark) {
-    var sumNull = 0;
+    let sumUndoneExams = 0;
     for (const typeExam in grades) {
         for (const exam in grades[typeExam]) {
-            if (grades[typeExam][exam] === null) sumNull += evaluation[typeExam][exam];
+            if (grades[typeExam][exam] === null) sumUndoneExams += evaluation[typeExam][exam];
         }
     }
-    return (5-finalMark)/sumNull;
+    return (5-finalMark)/sumUndoneExams;
 }
 
 function round(n) {
