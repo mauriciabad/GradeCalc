b1b98e552e43eaa4faf9a49e9d475a3591bd5e24
Maurici <hello@mauriciabad.com>
2019-06-09 13:11:56 +0200

    gulpfile uses browserify

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,12 @@ const babel = require('gulp-babel');
 const cleanCSS = require('gulp-clean-css');
 const minify = require('gulp-minify');
 const rename = require("gulp-rename");
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
+const browserify = require('browserify');
+const babelify = require('babelify');
+// const sourcemaps  = require('gulp-sourcemaps');
 
 gulp.task('css', () =>
   gulp.src('style.css')
@@ -21,9 +26,7 @@ gulp.task('css', () =>
 
 gulp.task('js', () =>
   gulp.src('script.js')
-    .pipe(babel({
-      presets: ['@babel/preset-env']   
-    }))
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
     .pipe(uglify())
     .pipe(rename((path) => {
       path.basename = "script.min";
@@ -31,9 +34,24 @@ gulp.task('js', () =>
     .pipe(gulp.dest('./'))
 );
 
+gulp.task('libs', () =>
+  browserify({entries: './libs.js', debug: true})
+      .transform("babelify", { presets: ["@babel/preset-env"] })
+      .bundle()
+      .pipe(source('libs.js'))
+      .pipe(buffer())
+      // .pipe(sourcemaps.init())
+      .pipe(uglify())
+      // .pipe(sourcemaps.write('./maps'))
+      .pipe(rename((path) => {
+        path.basename = "libs.min";
+      }))
+      .pipe(gulp.dest('./'))
+);
+
 gulp.task('watch', () => {
   gulp.watch('style.css', gulp.series('css'));
   gulp.watch('script.js', gulp.series('js'));
 });
 
-gulp.task('default', gulp.parallel('css','js'));
+gulp.task('default', gulp.parallel('css','js','libs'));
